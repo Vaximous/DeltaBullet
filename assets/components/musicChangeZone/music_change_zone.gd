8a54fd725c6840b4d1c2e3b86bfe7a68dce367e8
Vax <vaximous@gmail.com>
2025-01-05 03:06:13 -0500

    Update music_change_zone.gd

--- assets/components/musicChangeZone/music_change_zone.gd
+++ assets/components/musicChangeZone/music_change_zone.gd
@@ -7,17 +7,17 @@ extends Area3D
 var song_on_enter : AudioStream
 
 func _on_body_entered(body: Node3D) -> void:
-	if body is Player3D:
-		var music = AudioManager.get_audioplayer("music")
+	if body is BasePawn:
+		var music = musicManager.get_audioplayer("music")
 		if music != null:
 			song_on_enter = music.stream
-		AudioManager.save_stream_time()
-		AudioManager.create_audioplayer_with_stream(area_music, fade_time)
+		musicManager.save_stream_time()
+		musicManager.create_audioplayer_with_stream(area_music, fade_time)
 	pass # Replace with function body.
 
 
 func _on_body_exited(body: Node3D) -> void:
 	if switch_back_on_exit:
-		if body is Player3D:
-			AudioManager.create_audioplayer_with_stream(song_on_enter, fade_time)
+		if body is BasePawn:
+			musicManager.create_audioplayer_with_stream(song_on_enter, fade_time)
 	pass # Replace with function body.

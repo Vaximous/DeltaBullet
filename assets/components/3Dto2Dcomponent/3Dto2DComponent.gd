ef39936bf23b87f177494811cfa649d59f634111
toafloast <104696964+toafloast@users.noreply.github.com>
2025-08-11 13:27:34 -0700

    Update 3Dto2DComponent.gd

--- assets/components/3Dto2Dcomponent/3Dto2DComponent.gd
+++ assets/components/3Dto2Dcomponent/3Dto2DComponent.gd
@@ -1,21 +1,22 @@
 extends Marker3D
 
 
-static var control_nodes : Array[Control]
+var control_nodes : Array[Control]
 
 
 @export_subgroup("Behavior")
 @export var clamp_to_edge : bool = true
 
 
-func _process(delta: float) -> void:
+func _ready() -> void:
+	assign_control_children()
+
+
+func _process(_delta: float) -> void:
 	var cam = get_viewport().get_camera_3d()
 	if cam is Camera3D:
 		var unprojected_position = cam.unproject_position(global_position)
-		for n in get_children():
-			if n is AudioStreamPlayer or n is Area3D:
-				return
-
+		for n in control_nodes:
 			n.global_position = unprojected_position
 			n.visible = !cam.is_position_behind(global_position)
 			if clamp_to_edge:
@@ -27,12 +28,5 @@ func clamp_to_screen(item : Control) -> void:
 	item.global_position = item.global_position.clamp(Vector2.ZERO, view_rect.size - (item.size * item.scale))
 
 
-func _on_child_entered_tree(node: Node) -> void:
-	if !control_nodes.has(node):
-		if node is Control:
-			control_nodes.append(node)
-
-
-func _on_child_exiting_tree(node: Node) -> void:
-	if control_nodes.has(node):
-		control_nodes.erase(node)
+func assign_control_children() -> void:
+	control_nodes.assign(get_children())
